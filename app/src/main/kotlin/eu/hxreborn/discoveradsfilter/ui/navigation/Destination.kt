8142b88b5bf6f473e38d8086410a578c8766c79f
hxreborn <hxreborn@duck.com>
2026-04-25 10:33:51 +0200

    refactor(ui): filter licenses to direct deps, add scrollbar
    Signed-off-by: hxreborn <hxreborn@duck.com>

--- app/src/main/kotlin/eu/hxreborn/discoveradsfilter/ui/navigation/Destination.kt
+++ app/src/main/kotlin/eu/hxreborn/discoveradsfilter/ui/navigation/Destination.kt
@@ -4,11 +4,15 @@ import androidx.navigation3.runtime.NavKey
 import kotlinx.serialization.Serializable
 
 sealed interface Destination : NavKey {
-    @Serializable data object Dashboard : Destination
+    @Serializable
+    data object Dashboard : Destination
 
-    @Serializable data object Diagnostics : Destination
+    @Serializable
+    data object Diagnostics : Destination
 
-    @Serializable data object About : Destination
+    @Serializable
+    data object About : Destination
 
-    @Serializable data object Licenses : Destination
+    @Serializable
+    data object Licenses : Destination
 }
--- app/src/main/kotlin/eu/hxreborn/discoveradsfilter/ui/screen/LicensesScreen.kt
+++ app/src/main/kotlin/eu/hxreborn/discoveradsfilter/ui/screen/LicensesScreen.kt
@@ -3,6 +3,7 @@ package eu.hxreborn.discoveradsfilter.ui.screen
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,16 +15,56 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import com.mikepenz.aboutlibraries.Libs
+import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import eu.hxreborn.discoveradsfilter.R
+import eu.hxreborn.discoveradsfilter.ui.util.drawVerticalScrollbar
+import kotlinx.collections.immutable.toImmutableList
+
+private val DIRECT_DEPENDENCY_GROUPS =
+    setOf(
+        "io.github.libxposed",
+        "org.luckypray",
+        "me.zhanghai.compose.preference",
+        "org.jetbrains.kotlinx",
+        "com.github.topjohnwu.libsu",
+        "com.mikepenz",
+        "androidx.compose",
+        "androidx.compose.material3",
+        "androidx.compose.material",
+        "androidx.compose.ui",
+        "androidx.core",
+        "androidx.activity",
+        "androidx.lifecycle",
+        "androidx.navigation3",
+    )
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LicensesScreen(onBack: () -> Unit) {
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    val libs by produceLibraries()
+    val listState = rememberLazyListState()
+
+    val filtered =
+        remember(libs) {
+            libs?.let { l ->
+                val directLibs =
+                    l.libraries.filter { lib ->
+                        DIRECT_DEPENDENCY_GROUPS.any { lib.uniqueId.startsWith(it) }
+                    }
+                Libs(
+                    libraries = directLibs.toImmutableList(),
+                    licenses = l.licenses,
+                )
+            }
+        }
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -54,7 +95,9 @@ fun LicensesScreen(onBack: () -> Unit) {
         contentWindowInsets = WindowInsets(0),
     ) { innerPadding ->
         LibrariesContainer(
-            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            libraries = filtered,
+            modifier = Modifier.fillMaxSize().padding(innerPadding).drawVerticalScrollbar(listState),
+            lazyListState = listState,
         )
     }
 }
